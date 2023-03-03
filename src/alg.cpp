764c534ba21e89f947ba067aefd46f383448931f
Semen-Chugunov <125124591+Semen-Chugunov@users.noreply.github.com>
2023-03-03 15:41:01 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -32,17 +32,15 @@ double expn(double x, uint16_t count) {
 }
 
 double sinn(double x, uint16_t count) {
-  double r = 0;
-  for (uint16_t i = 1; i <= count; i++) {
-    r += pown(-1, i - 1) * (pown(x, 2 * i - 1) / fact(2 * i - 1));
-  }
+  double r = x;
+  for (uint16_t i = 2; i <= count; i++)
+    r += pown(-1, i - 1) * calcItem(x, 2 * i - 1);
   return r;
 }
 
 double cosn(double x, uint16_t count) {
-  double r = 0;
-  for (uint16_t i = 1; i <= count; i++) {
-    r += pown(-1, i - 1) * (pown(x, 2 * i - 2) / fact(2 * i - 2));
-  }
+  double r = 1;
+  for (uint16_t i = 2; i <= count; i++)
+    r += pown(-1, i - 1) * calcItem(x, 2 * i - 2);
   return r;
 }
